dc21d6d6b8ecb2fe305b730377cc5f3a6022274b
aml2507 <antonellalesta@gmail.com>
2024-03-30 19:19:58 +0100

    Structure and main.js file

---
+++ frontend/main.js
@@ -0,0 +1,19 @@
+//Function get called, when content loaded run the event visitCount
+window.addEventListener('DOMContentLoaded'), (event) => {
+    getVisitCount();
+}
+const functionApi = '';
+
+const  getVisitCount = ()=> {
+    let count =30;
+    fetch(functionApi).then(response =>{
+        return response.json()
+    }).then(response => {
+        console.log ("Website called function Api");
+        count = response.count
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
